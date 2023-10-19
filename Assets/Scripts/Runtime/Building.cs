cb5155e45f71d0e59e89b0b07e538461f5c9594d
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-10-19 22:43:45 +0700

    Added Lumberjack's Hut

---
+++ Assets/Scripts/Runtime/Building.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace BFG.Runtime {
+[CreateAssetMenu(fileName = "Data", menuName = "Gameplay/Building", order = 1)]
+public class Building : ScriptableObject {
+    [SerializeField]
+    string _codename;
+
+    [SerializeField]
+    string _harvestTileCodename;
+
+    [SerializeField]
+    TileBase _tile;
+
+    public string codename => _codename;
+    public string harvestTileCodename => _harvestTileCodename;
+
+    public TileBase tile => _tile;
+}
+}
