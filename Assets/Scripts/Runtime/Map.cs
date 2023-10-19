57cbcb4e34ecc8f0c9989c945b8dca4a8d08ee60
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-10-19 14:52:20 +0700

    Separated map from renderer & fixed cliffs placement

--- Assets/Scripts/Runtime/Map.cs
+++ Assets/Scripts/Runtime/Map.cs
@@ -1,9 +1,8 @@
-using System;
 using System.Collections.Generic;
 using SimplexNoise;
 using Sirenix.OdinInspector;
 using UnityEngine;
-using UnityEngine.Tilemaps;
+using UnityEngine.Events;
 
 namespace BFG.Runtime {
 public struct Tile {
@@ -12,10 +11,6 @@ public struct Tile {
 }
 
 public class Map : MonoBehaviour {
-    const string TerrainTilemapNameTemplate = "Gen-Terrain";
-    const string BuildingsTilemapNameTemplate = "Gen-Buildings";
-    const string ResourcesTilemapNameTemplate = "Gen-Resources";
-
     // Layers:
     // 1 - Terrain (depends on height)
     // 2 - Stone, Trees, Ore, Buildings, Rails
@@ -24,29 +19,7 @@ public class Map : MonoBehaviour {
     //
     // Stone, Tree, Ore:
 
-    [Header("Dependencies")]
-    [SerializeField]
-    [Required]
-    Grid _grid;
-
-    [SerializeField]
-    [Required]
-    TileBase _tileGrass;
-
-    [SerializeField]
-    TileBase _tileRoad;
-
-    [SerializeField]
-    [Required]
-    TileBase _tileForest;
-
-    [SerializeField]
-    [Required]
-    GameObject _tilemapPrefab;
-
     [Header("Configuration")]
-    // [SerializeField]
-    // int _randomSeed;
     [SerializeField]
     [Min(1)]
     int _mapSizeX = 10;
@@ -75,8 +48,16 @@ public class Map : MonoBehaviour {
     [Min(0)]
     int _maxHeight = 1;
 
-    List<List<int>> _tileHeights;
-    List<List<Tile>> _tiles;
+    [Header("Setup")]
+    [SerializeField]
+    public UnityEvent OnTilesRegenerated;
+
+    public int sizeY => _mapSizeY;
+    public int sizeX => _mapSizeX;
+
+    // NOTE(Hulvdan): Indexes start from the bottom left corner and go to the top right one
+    public List<List<Tile>> tiles { get; private set; }
+    public List<List<int>> tileHeights { get; private set; }
 
     void Awake() {
         RegenerateTilemap();
@@ -90,12 +71,13 @@ public class Map : MonoBehaviour {
 
     [Button("RegenerateTilemap")]
     void RegenerateTilemap() {
-        _tiles = new List<List<Tile>>();
-        _tileHeights = new List<List<int>>();
+        tiles = new List<List<Tile>>();
+        tileHeights = new List<List<int>>();
 
+        // NOTE(Hulvdan): Generating tiles
         for (var y = 0; y < _mapSizeY; y++) {
-            var tiles = new List<Tile>();
-            var tileHeights = new List<int>();
+            var tilesRow = new List<Tile>();
+            var tileHeightsRow = new List<int>();
 
             for (var x = 0; x < _mapSizeX; x++) {
                 var tile = new Tile {
@@ -103,98 +85,33 @@ public class Map : MonoBehaviour {
                     HasForest = MakeSomeNoise2D(_randomSeed, x, y, _forestNoiseScale) >
                                 _forestThreshold
                 };
-                tiles.Add(tile);
+                tilesRow.Add(tile);
 
                 var randomH = MakeSomeNoise2D(_randomSeed, x, y, _terrainHeightNoiseScale) *
                               (_maxHeight + 1);
-                tileHeights.Add(Mathf.Min(_maxHeight, (int)randomH));
+                tileHeightsRow.Add(Mathf.Min(_maxHeight, (int)randomH));
             }
 
-            _tiles.Add(tiles);
-            _tileHeights.Add(tileHeights);
+            tiles.Add(tilesRow);
+            tileHeights.Add(tileHeightsRow);
         }
 
-        DeleteOldTilemaps();
-        RegenerateTilemapGameObject();
+        for (var y = 0; y < _mapSizeY; y++) {
+            for (var x = 0; x < _mapSizeX; x++) {
+                if (y == 0 || tileHeights[y][x] > tileHeights[y - 1][x]) {
+                    var s = tiles[y][x];
+                    s.Name = "cliff";
+                    tiles[y][x] = s;
+                }
+            }
+        }
+
+        OnTilesRegenerated?.Invoke();
     }
 
     float MakeSomeNoise2D(int seed, int x, int y, float scale) {
         Noise.Seed = seed;
         return Noise.CalcPixel2D(x, y, scale) / 255f;
-    }
-
-    void DeleteOldTilemaps() {
-        foreach (Transform child in _grid.transform) {
-            if (child.gameObject.name.StartsWith(TerrainTilemapNameTemplate)
-                || child.gameObject.name.StartsWith(BuildingsTilemapNameTemplate)
-                || child.gameObject.name.StartsWith(ResourcesTilemapNameTemplate)) {
-                child.gameObject.SetActive(false);
-            }
-        }
-    }
-
-    void RegenerateTilemapGameObject() {
-        var maxHeight = 0;
-        for (var y = 0; y < _mapSizeY; y++) {
-            for (var x = 0; x < _mapSizeX; x++) {
-                maxHeight = Math.Max(maxHeight, _tileHeights[y][x]);
-            }
-        }
-
-        // Terrain 0 (y=0)
-        // Terrain 1 (y=-0.001)
-        // Terrain 2 (y=-0.002)
-        // Buildings 2 (y=-0.0021)
-        var terrainMaps = new List<Tilemap>();
-        for (var i = 0; i <= maxHeight; i++) {
-            var terrain = GenerateTilemap(i, -i / 1000f, TerrainTilemapNameTemplate);
-            terrainMaps.Add(terrain.GetComponent<Tilemap>());
-        }
-
-        var buildings = GenerateTilemap(0, -maxHeight - 1 / 1000f, BuildingsTilemapNameTemplate)
-            .GetComponent<Tilemap>();
-        terrainMaps.Add(buildings);
-
-        var resources = GenerateTilemap(0, -maxHeight - 1 / 1000f, ResourcesTilemapNameTemplate)
-            .GetComponent<Tilemap>();
-        terrainMaps.Add(resources);
-
-        for (var h = 0; h <= maxHeight; h++) {
-            for (var y = 0; y < _mapSizeY; y++) {
-                for (var x = 0; x < _mapSizeX; x++) {
-                    if (h == 0
-                        || _tileHeights[y][x] == h
-                        || (y > 0 && _tileHeights[y - 1][x] == h)) {
-                        terrainMaps[h].SetTile(new Vector3Int(x, y, 0), _tileGrass);
-                    }
-
-                    if (h == 0
-                        && (
-                            _tiles[y][x].HasForest
-                            || (y < _tiles.Count - 1 && _tiles[y + 1][x].HasForest)
-                        )
-                        && TileIsNotACliff(x, y)
-                       ) {
-                        resources.SetTile(new Vector3Int(x, y + 1, 0), _tileForest);
-                    }
-                }
-            }
-        }
-    }
-
-    bool TileIsNotACliff(int x, int y) {
-        if (y >= _tiles.Count - 1) {
-            return true;
-        }
-
-        return _tileHeights[y][x] < _tileHeights[y + 1][x];
-    }
-
-    GameObject GenerateTilemap(int i, float y, string nameTemplate) {
-        var terrainTilemap = Instantiate(_tilemapPrefab, _grid.transform);
-        terrainTilemap.name = nameTemplate + i;
-        terrainTilemap.transform.position = new Vector3(0, y, 0);
-        return terrainTilemap;
     }
 }
 }
