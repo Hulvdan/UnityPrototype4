4ecfbd1127996bb8502e72538efd2043bb0c7cd2
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-10-21 01:13:44 +0700

    Added Warehouse & Sawmill

--- Assets/Scripts/Runtime/Scriptables/ScriptableBuilding.cs
+++ Assets/Scripts/Runtime/Scriptables/ScriptableBuilding.cs
@@ -13,18 +13,27 @@ public class ScriptableBuilding : ScriptableObject {
     BuildingType _type;
 
     [SerializeField]
+    [ShowIf("_type", BuildingType.Harvest)]
     string _harvestResourceCodename;
 
     [SerializeField]
+    [ShowIf("_type", BuildingType.Harvest)]
     [Min(0)]
     int _cellsRadius;
 
     [SerializeField]
+    [ShowIf("_type", BuildingType.Store)]
+    [Min(1)]
+    int _storeItemsAmount = 1;
+
+    [SerializeField]
+    [PreviewField]
     TileBase _tile;
 
     public string codename => _codename;
     public string harvestResourceCodename => _harvestResourceCodename;
     public int cellsRadius => _cellsRadius;
+    public int storeItemsAmount => _storeItemsAmount;
 
     public TileBase tile => _tile;
 }
