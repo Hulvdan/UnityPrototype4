1359edc0023bb86dda18ff546006fc317bd1fd6f
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-10-21 00:59:24 +0700

    Renamed GameplayUIManager to UIManager

--- Assets/Scripts/Runtime/UIManager.cs
+++ Assets/Scripts/Runtime/UIManager.cs
@@ -3,7 +3,7 @@ using Sirenix.OdinInspector;
 using TMPro;
 using UnityEngine;
 
-public class GameplayUIManager : MonoBehaviour {
+public class UIManager : MonoBehaviour {
     [Header("Dependencies")]
     [SerializeField]
     [Required]
