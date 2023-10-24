630b1f0dd1ed7a163e051c0741ceb0c9a010ef34
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-10-24 17:56:27 +0700

    Added arrows

---
+++ Assets/Scripts/Runtime/HorseMovementSystem.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace BFG.Runtime {
+public class MapNode {
+    public Vector2Int Position => Vector2Int.zero;
+}
+
+public class TrainNode {
+    public Vector2 Position {
+        get => Vector2.zero;
+        set { }
+    }
+}
+
+public enum ChainNodeType {
+    Horse,
+    Cart
+}
+
+public class MovementChainNode {
+    public ChainNodeType Type;
+    public MovementChainNode Previous;
+    public float Width = 0.8f; // Max is 1!
+}
+
+public class HorseMovementSystem {
+    List<TrainNode> _trainNodes;
+    float _speed = 1f;
+    float _duration = 1f;
+
+    // MovementNode
+
+    void Update() {
+        foreach (var node in _trainNodes) {
+            // node.Position +=
+        }
+    }
+}
+}
---
+++ Assets/Scripts/Runtime/HorseMovementSystemInterface.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace BFG.Runtime {
+public class HorseMovementSystemInterface : MonoBehaviour {
+    HorseMovementSystem _system;
+
+    public List<GameObject> Nodes = new();
+
+    [SerializeField]
+    Sprite _arrow0;
+
+    [SerializeField]
+    Sprite _arrow1;
+
+    [SerializeField]
+    Sprite _arrow21;
+
+    [SerializeField]
+    Sprite _arrow22;
+
+    [SerializeField]
+    Sprite _arrow3;
+
+    [SerializeField]
+    Sprite _arrow4;
+
+    [Button("Generate system with nodes")]
+    void GenerateSystemWithNodes() {
+    }
+}
+}
