bf5308ea27609b4b137d87d3d0275bcbf6ac321e
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-10-28 01:25:30 +0700

    Selectable button in the sidebar

---
+++ Assets/Scripts/Runtime/UI/BuildablesPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BFG.Runtime {
+public class BuildablesPanel : MonoBehaviour {
+    [SerializeField]
+    List<BuildableButton> _buttons;
+
+    void Start() {
+        foreach (var button in _buttons) {
+            button.Init(this);
+        }
+    }
+
+    public void OnButtonSelected(int buttonInstanceID) {
+        foreach (var button in _buttons) {
+            if (button.GetInstanceID() != buttonInstanceID) {
+                button.SetSelected(false);
+            }
+        }
+    }
+}
+}
