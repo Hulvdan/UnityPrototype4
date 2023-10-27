46ca30f565939b653e0b9a1d34b23afadabc88cd
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-10-28 04:31:33 +0700

    Togglable buttons of buildables

--- Assets/Scripts/Runtime/UI/BuildablesPanel.cs
+++ Assets/Scripts/Runtime/UI/BuildablesPanel.cs
@@ -11,20 +11,33 @@ public class BuildablesPanel : MonoBehaviour {
     [Required]
     Map _map;
 
+    bool _ignoreButtonStateChanges;
+
     void Start() {
         foreach (var button in _buttons) {
             button.Init(this);
         }
     }
 
-    public void OnButtonSelected(int buttonInstanceID) {
+    public void OnButtonChangedSelectedState(int? selectedButtonInstanceID) {
+        if (_ignoreButtonStateChanges) {
+            return;
+        }
+
+        _ignoreButtonStateChanges = true;
         foreach (var button in _buttons) {
-            if (button.GetInstanceID() == buttonInstanceID) {
+            if (button.GetInstanceID() == selectedButtonInstanceID) {
                 _map.SetSelectedItem(button.item);
                 continue;
             }
 
             button.SetSelected(false);
+        }
+
+        _ignoreButtonStateChanges = false;
+
+        if (selectedButtonInstanceID == null) {
+            _map.SetSelectedItem(SelectedItem.None);
         }
     }
 }
