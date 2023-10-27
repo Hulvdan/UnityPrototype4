d522b38f05e356f0d5769f20d883240aaa3f9f7f
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-10-28 02:01:27 +0700

    Settings selected item in Map

--- Assets/Scripts/Runtime/UI/BuildablesPanel.cs
+++ Assets/Scripts/Runtime/UI/BuildablesPanel.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityEngine;
 
 namespace BFG.Runtime {
 public class BuildablesPanel : MonoBehaviour {
     [SerializeField]
     List<BuildableButton> _buttons;
+
+    [SerializeField]
+    [Required]
+    Map _map;
 
     void Start() {
         foreach (var button in _buttons) {
@@ -16,6 +21,9 @@ public class BuildablesPanel : MonoBehaviour {
         foreach (var button in _buttons) {
             if (button.GetInstanceID() != buttonInstanceID) {
                 button.SetSelected(false);
+            }
+            else {
+                _map.SetSelectedItem(button.item);
             }
         }
     }
