4dd8ba9838fcb1b4829979ee63a22d4d164f5744
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-10-28 19:11:38 +0700

    Refactorings
    Renamed Tile to TerrainTile;
    Renamed MapCell to ElementTile

---
+++ Assets/Scripts/Runtime/ElementTile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace BFG.Runtime {
+/// <summary>
+/// Is it a road or lumberjack's hut?
+/// </summary>
+public struct ElementTile {
+    public ElementTileType Type;
+    public int Rotation;
+
+    public ElementTile(ElementTileType type, int rotation) {
+        if ((type == ElementTileType.Road || type == ElementTileType.None) && rotation != 0) {
+            Debug.LogError("WTF IS GOING ON HERE?");
+            rotation = 0;
+        }
+
+        Type = type;
+        Rotation = rotation;
+    }
+
+    public static ElementTile None = new(ElementTileType.None, 0);
+    public static ElementTile Road = new(ElementTileType.Road, 0);
+}
+}
