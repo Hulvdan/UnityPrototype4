a90fdf5c3113ec9e040502d4840455ec4efdc6d6
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-11-12 13:46:15 +0700

    Localization

---
+++ Assets/Scripts/Editor/GStringKeySelectorWindow.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BFG.Runtime;
+using UnityEditor;
+using UnityEngine;
+
+namespace Editor {
+public class GStringKeySelectorWindow : ScriptableWizard {
+    readonly Rect _gridRect = new(0, 0, 300, 60);
+
+    Dictionary<string, LocalizationRecord> _strings;
+    List<string> _stringsHuman = new();
+
+    [NonSerialized]
+    public string InitialKey;
+
+    [NonSerialized]
+    public Action<string> OnKeyChanged = delegate { };
+
+    int _selected;
+
+    void Awake() {
+        Load();
+    }
+
+    void OnGUI() {
+        GUILayout.BeginScrollView(Vector2.zero);
+
+        var newSelected = GUI.SelectionGrid(_gridRect, _selected, _stringsHuman.ToArray(), 1);
+        if (_selected != newSelected) {
+            _selected = newSelected;
+            OnKeyChanged?.Invoke(_strings.Keys.ToArray()[_selected]);
+            Close();
+        }
+
+        GUILayout.EndScrollView();
+    }
+
+    void Load() {
+        _strings = new LocalizationDatabaseLoader().Load();
+        _stringsHuman = _strings.Select(i => $"{i.Key} (ex. {i.Value.En})").ToList();
+
+        _selected = -1;
+        if (!string.IsNullOrEmpty(InitialKey)) {
+            _selected = _strings.Keys.ToList().IndexOf(InitialKey);
+        }
+    }
+}
+}
---
+++ Assets/Scripts/Runtime/UI/LocalizationRecord.cs
@@ -0,0 +1,6 @@
+﻿namespace BFG.Runtime {
+public class LocalizationRecord {
+    public string En { get; set; }
+    public string Ru { get; set; }
+}
+}
