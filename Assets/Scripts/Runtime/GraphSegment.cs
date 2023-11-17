e22fb1ff0b52101bd6dd56e981d2e54a723abb12
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-11-18 00:52:55 +0700

    Removed Vertex & Tiles duplication from the algorithm

--- Assets/Scripts/Runtime/GraphSegment.cs
+++ Assets/Scripts/Runtime/GraphSegment.cs
@@ -1,14 +1,116 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace BFG.Runtime {
-public class GraphSegment {
-    public List<GraphVertex> Vertexes;
-    public List<Vector2Int> MovementTiles;
+public class GraphSegment : IComparable<GraphSegment>, IEquatable<GraphSegment> {
+    public readonly List<GraphVertex> Vertexes;
+    public readonly List<Vector2Int> MovementTiles;
 
     public GraphSegment(List<GraphVertex> vertexes, List<Vector2Int> movementTiles) {
         Vertexes = vertexes;
         MovementTiles = movementTiles;
+
+        vertexes.Sort();
+        movementTiles.Sort(Utils.StupidVector2IntComparation);
+
+        // Duplication Checks
+        for (var i = 0; i < vertexes.Count; i++) {
+            for (var j = 0; j < vertexes.Count; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                Assert.AreNotEqual(vertexes[i], vertexes[j]);
+            }
+        }
+
+        for (var i = 0; i < movementTiles.Count; i++) {
+            for (var j = 0; j < movementTiles.Count; j++) {
+                if (i == j) {
+                    continue;
+                }
+
+                Assert.AreNotEqual(movementTiles[i], movementTiles[j]);
+            }
+        }
+    }
+
+    public bool Equals(GraphSegment other) {
+        if (ReferenceEquals(null, other)) {
+            return false;
+        }
+
+        if (ReferenceEquals(this, other)) {
+            return true;
+        }
+
+        return Equals(Vertexes, other.Vertexes) && Equals(MovementTiles, other.MovementTiles);
+    }
+
+    public int CompareTo(GraphSegment other) {
+        if (Vertexes.Count > other.Vertexes.Count) {
+            return 1;
+        }
+
+        if (Vertexes.Count < other.Vertexes.Count) {
+            return -1;
+        }
+
+        if (MovementTiles.Count > other.MovementTiles.Count) {
+            return 1;
+        }
+
+        if (MovementTiles.Count > other.MovementTiles.Count) {
+            return -1;
+        }
+
+        for (var i = 0; i < Vertexes.Count; i++) {
+            var cmp = Vertexes[i].CompareTo(other.Vertexes[i]);
+            if (cmp != 0) {
+                return cmp;
+            }
+        }
+
+        for (var i = 0; i < MovementTiles.Count; i++) {
+            var cmp = Utils.StupidVector2IntComparation(MovementTiles[i], other.MovementTiles[i]);
+            if (cmp != 0) {
+                return cmp;
+            }
+        }
+
+        return 0;
+    }
+
+    public override bool Equals(object obj) {
+        if (ReferenceEquals(null, obj)) {
+            return false;
+        }
+
+        if (ReferenceEquals(this, obj)) {
+            return true;
+        }
+
+        if (obj.GetType() != GetType()) {
+            return false;
+        }
+
+        return Equals((GraphSegment)obj);
+    }
+
+    public override int GetHashCode() {
+        return HashCode.Combine(Vertexes, MovementTiles);
+    }
+
+    public override string ToString() {
+        var str = "(Vertexes: [";
+        str += string.Join(", ", Vertexes);
+        str += "], MovementTiles: [";
+        str += string.Join(", ", MovementTiles);
+        str += "])";
+        return str;
+        // return $"(Vertexes: {Vertexes}, MovementTiles: {MovementTiles})";
     }
 }
 }
--- Assets/Scripts/Runtime/Utils.cs
+++ Assets/Scripts/Runtime/Utils.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Random = System.Random;
 
 namespace BFG.Runtime {
 public static class Utils {
@@ -8,6 +9,26 @@ public static class Utils {
             var randomIndex = random.Next(arr.Count);
             (arr[randomIndex], arr[i]) = (arr[i], arr[randomIndex]);
         }
+    }
+
+    public static int StupidVector2IntComparation(Vector2Int a, Vector2Int b) {
+        if (a.x > b.x) {
+            return 1;
+        }
+
+        if (a.x < b.x) {
+            return -1;
+        }
+
+        if (a.y > b.y) {
+            return 1;
+        }
+
+        if (a.y < b.y) {
+            return -1;
+        }
+
+        return 0;
     }
 }
 }
