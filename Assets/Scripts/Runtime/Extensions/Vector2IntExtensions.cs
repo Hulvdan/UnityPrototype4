f9297781b7c664975bbce91d38ce0f3d9f1e27a2
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-11-19 22:23:06 +0700

    Added useful extension methods by git-amend@YouTube

---
+++ Assets/Scripts/Runtime/Extensions/Vector2IntExtensions.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace BFG.Runtime.Extensions {
+public static class Vector2IntExtensions {
+    /// <summary>
+    ///     Sets any x y values of a Vector2Int.
+    /// </summary>
+    /// <example>
+    ///     <code>
+    ///     // (1, 2)
+    ///     var vector = Vector2Int.one.With(y: 2);
+    ///     </code>
+    /// </example>
+    public static Vector2Int With(this Vector2Int vector, int? x = null, int? y = null) {
+        return new(
+            x ?? vector.x,
+            y ?? vector.y
+        );
+    }
+
+    /// <summary>
+    ///     Adds to any x y values of a Vector2Int
+    /// </summary>
+    /// <example>
+    ///     <code>
+    ///     // (1, 2)
+    ///     var vector = Vector2Int.one.Add(y: 1);
+    ///     </code>
+    /// </example>
+    public static Vector2Int Add(this Vector2Int vector, int? x = null, int? y = null) {
+        return new(
+            vector.x + (x ?? 0),
+            vector.y + (y ?? 0)
+        );
+    }
+}
+}
---
+++ Assets/Scripts/Runtime/Extensions/Vector3IntExtensions.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+namespace BFG.Runtime.Extensions {
+public static class Vector3IntExtensions {
+    /// <summary>
+    ///     Sets any x y z values of a Vector3Int.
+    /// </summary>
+    /// <example>
+    ///     <code>
+    ///     // (1, 2, 1)
+    ///     var vector = Vector3Int.one.With(y: 2);
+    ///     </code>
+    /// </example>
+    public static Vector3Int With(
+        this Vector3Int vector, int? x = null, int? y = null, int? z = null
+    ) {
+        return new(
+            x ?? vector.x,
+            y ?? vector.y,
+            z ?? vector.z
+        );
+    }
+
+    /// <summary>
+    ///     Adds to any x y z values of a Vector3Int
+    /// </summary>
+    /// <example>
+    ///     <code>
+    ///     // (1, 2, 1)
+    ///     var vector = Vector3Int.one.Add(y: 1);
+    ///     </code>
+    /// </example>
+    public static Vector3Int Add(
+        this Vector3Int vector, int? x = null, int? y = null, int? z = null
+    ) {
+        return new(
+            vector.x + (x ?? 0),
+            vector.y + (y ?? 0),
+            vector.z + (z ?? 0)
+        );
+    }
+}
+}
