33ff79601c4b4cdbfc40dcd0663064fd1d19f45e
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-11-28 13:01:32 +0700

    Prototyping arcade-ish movement

---
+++ Assets/Scripts/Prototyping/ScaleMovementFeedback.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace BFG.Prototyping {
+public sealed class ScaleMovementFeedback : MovementFeedback {
+    public override void UpdateData(float dt, float normalized, float coef, HumanBinding binding) {
+    }
+}
+}
