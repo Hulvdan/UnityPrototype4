3be948159945f095caf058e808ed6bd9c90c2c64
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-11-28 14:02:09 +0700

    Created a prototyping scene with 40 humans for creating and testing movement feedbacks

---
+++ Assets/Scripts/Prototyping/RotationMovementFeedback.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace BFG.Prototyping {
+public sealed class RotationMovementFeedback : MovementFeedback {
+    [SerializeField]
+    Vector2 _amplitudeZ = new Vector2(-1, 1) * 20;
+
+    public override void UpdateData(float dt, float normalized, float coef, HumanBinding binding) {
+        var rot = Mathf.Lerp(_amplitudeZ.x, _amplitudeZ.y, coef);
+
+        var tr = binding.Human.transform;
+        var trRotation = tr.localRotation;
+        var euler = trRotation.eulerAngles;
+        euler.z = rot;
+        trRotation.eulerAngles = euler;
+        tr.localRotation = trRotation;
+    }
+}
+}
--- Assets/Scripts/Prototyping/ScaleMovementFeedback.cs
+++ Assets/Scripts/Prototyping/ScaleMovementFeedback.cs
@@ -1,9 +1,22 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace BFG.Prototyping {
 public sealed class ScaleMovementFeedback : MovementFeedback {
+    [SerializeField]
+    Vector2 _amplitudeX = new(0.9f, 1.1f);
+
+    [SerializeField]
+    Vector2 _amplitudeY = new(0.9f, 1.1f);
+
     public override void UpdateData(float dt, float normalized, float coef, HumanBinding binding) {
+        var scaleX = Mathf.Lerp(_amplitudeX.x, _amplitudeX.y, coef);
+        var scaleY = Mathf.Lerp(_amplitudeY.x, _amplitudeY.y, coef);
+
+        var tr = binding.Human.transform;
+        var trScale = tr.localScale;
+        trScale.x = scaleX;
+        trScale.y = scaleY;
+        tr.localScale = trScale;
     }
 }
 }
