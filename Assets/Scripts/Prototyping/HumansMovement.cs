6b3a671c4ce6b54c19d281dd8cab25b032b760b9
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-11-28 14:02:09 +0700

    Created a prototyping scene with 40 humans for creating and testing movement feedbacks

--- Assets/Scripts/Prototyping/HumansMovement.cs
+++ Assets/Scripts/Prototyping/HumansMovement.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using BFG.Core;
+using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.Assertions;
 
@@ -95,6 +97,19 @@ public class HumansMovement : MonoBehaviour {
 
     public List<HumanBinding> Bindings = new();
 
+    public int Rows = 9;
+    public int Columns = 4;
+    public int ColumnsGap = 1;
+    public int RowsGap = 1;
+    public int StartingRow = 0;
+    public int StartingColumn = 0;
+    public int TravelDistanceX = 2;
+    public int TravelDistanceY = 0;
+
+    public Transform humansContainer;
+    public GameObject humanPrefab;
+    public List<MovementFeedback> defaultMovementFeedbacks;
+
     [NonSerialized]
     float _movementElapsed;
 
@@ -129,6 +144,44 @@ public class HumansMovement : MonoBehaviour {
                 var feedback = binding.Pattern.Feedbacks[i];
                 feedback.UpdateData(Time.deltaTime, normalized, coef, binding);
             }
+        }
+    }
+
+    [Button]
+    public void RegenerateHumans() {
+        foreach (Transform human in humansContainer) {
+            human.gameObject.SetActive(false);
+        }
+
+        Bindings.Clear();
+
+        var index = 0;
+        var x = StartingColumn;
+        for (var column = 0; column < Columns; column++) {
+            var y = StartingRow;
+            for (var row = 0; row < Rows; row++) {
+                var human = Instantiate(humanPrefab, humansContainer);
+                human.name = $"Human.{index++}";
+
+                human.transform.localPosition = new Vector3(x, y, 0) + new Vector3(1, 1, 0) / 2;
+
+                var path = new List<Vector2Int> {
+                    new(x, y),
+                    new Vector2Int(x, y) + new Vector2Int(TravelDistanceX, TravelDistanceY),
+                };
+
+                Bindings.Add(new() {
+                    Human = human,
+                    Path = path,
+                    Pattern = new() {
+                        Feedbacks = defaultMovementFeedbacks.Select(i => i).ToList(),
+                    },
+                });
+
+                y += RowsGap + 1;
+            }
+
+            x += ColumnsGap + 1;
         }
     }
 }
