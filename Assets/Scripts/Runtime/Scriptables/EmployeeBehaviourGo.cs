f7a899f9066d718425bc1a98a7646c06256189a9
Aleksei Chistov <aleksey.chistov@gmail.com>
2023-12-11 20:17:05 +0700

    Fixed (un-)booking behaviour

--- Assets/Scripts/Runtime/Scriptables/EmployeeBehaviourGo.cs
+++ Assets/Scripts/Runtime/Scriptables/EmployeeBehaviourGo.cs
@@ -2,6 +2,8 @@
 using System;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.Serialization;
 
 namespace BFG.Runtime.Entities {
 public enum EmployeeBehaviourGoType {
@@ -20,12 +22,25 @@ public sealed class EmployeeBehaviourGo {
     [ShowIf("_type", EmployeeBehaviourGoType.GoToDestination)]
     HumanDestinationType _destinationType;
 
+    [SerializeField]
+    [ShowIf("_type", EmployeeBehaviourGoType.GoToDestination)]
+    [Min(-1)]
+    int _bookingTileBehaviourId = -1;
+
+    [SerializeField]
+    [ShowIf("_type", EmployeeBehaviourGoType.Processing)]
+    [Min(-1)]
+    int _unbookingTileBehaviourId = -1;
+
     public EmployeeBehaviour ToEmployeeBehaviour() {
         switch (_type) {
             case EmployeeBehaviourGoType.GoToDestination:
-                return new GoToDestinationEmployeeBehaviour(_destinationType);
+                return new GoToDestinationEmployeeBehaviour(
+                    _destinationType,
+                    _bookingTileBehaviourId == -1 ? null : _bookingTileBehaviourId
+                );
             case EmployeeBehaviourGoType.Processing:
-                return new ProcessingEmployeeBehaviour();
+                return new ProcessingEmployeeBehaviour(_unbookingTileBehaviourId);
             case EmployeeBehaviourGoType.PickingUpHarvestedResource:
                 return new PickingUpHarvestedResourceEmployeeBehaviour();
             case EmployeeBehaviourGoType.PlacingHarvestedResource:
